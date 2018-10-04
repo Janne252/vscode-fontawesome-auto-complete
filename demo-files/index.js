10db7dae2d664e01388a4eae381c7179d7c8707c
Janne <accounts@janne252.com>
2018-10-04 19:08:28 +0300

    Demo files

--- demo-files/index.js
+++ demo-files/index.js
@@ -2,4 +2,4 @@ let thing = 'foo';
 
 let favorite = 'color';
 
-let icon = 'far fa-address-book far fa-fas fab fab fas fa-phone  fas fa-chevron-circle-down  fab fa-laravel  far fa-address-book  fas fa-chevron-circle-down'
+let icon = 'far fa-address-book far fa-fas fab fab fas fa-phone  fas fa-chevron-circle-down  fab fa-laravel  far fa-address-book  fas fa-chevron-circle-down fa-far fa-user far fa-user  far fa-user'
