e6d984309f96183c8d656e684ac2df808bbfb0bd
Janne <accounts@janne252.com>
2018-10-04 19:02:58 +0300

    Demo files

--- demo-files/index.js
+++ demo-files/index.js
@@ -2,4 +2,4 @@ let thing = 'foo';
 
 let favorite = 'color';
 
-let icon = 'fa- fab fa-500px'
+let icon = 'far fa-address-book far fa-fas fab fab fas fa-phone  fas fa-chevron-circle-down  fab fa-laravel  far fa-address-book  fas fa-chevron-circle-down'
