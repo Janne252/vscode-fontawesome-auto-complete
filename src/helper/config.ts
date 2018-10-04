adca85f4f42ab8cac467e4194ca157b179892089
Janne <accounts@janne252.com>
2018-10-04 19:05:30 +0300

    Config helper

---
+++ src/helper/config.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param obj The object to get the property from. Can be an object or an array.
+ * @param key The key of the property.
+ * @param defaultValue Default value used if the obj is null or if the key doesn't exist in the object.
+ */
+export function getOrDefault<T = string, TObj = Object | Array<T>>(obj: TObj, key: PropertyKey, defaultValue?: T): T {
+    if (obj == null || key in obj === false) {
+        return defaultValue as T;
+    } 
+
+    return obj[key as keyof TObj] as any;
+}
